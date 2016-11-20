5c94ee68c1670b67d01825787ee6856e97c9253a
akshat1 <akshat.sh@gmail.com>
2016-11-20 20:20:08 +0000

    Disabled "strict" rule in postcss files, since they too run untranspiled (and thus need strict mode).

--- postcss/mixins/index.js
+++ postcss/mixins/index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'; // eslint-disable-line strict
 
 module.exports = {
   mixins: Object.assign(
--- postcss/mixins/text.js
+++ postcss/mixins/text.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'; // eslint-disable-line strict
 
 function textOverflow(mixin, overflowCharacter) {
   return {
